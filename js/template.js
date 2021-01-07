448a15a72b403f46a53b2a2176ae42e7cc5dda39
Striker Eureka <pablocaceros@gmail.com>
2021-01-07 16:27:44 -0600

    Se agrego codigo responsivo para las tarjetas

--- js/template.js
+++ js/template.js
@@ -1,28 +1,31 @@
 const templateCard = document.getElementById('card1');
 const templateCardPromise = document.getElementById('card2');
 const templateCardAsync = document.getElementById('card3');
-const state = document.getElementById('status');
+
+
+
 
 
 const sendTemplate = (object, i, typecall) => {
     const card = document.createElement('div');
 
     card.innerHTML = `
-    <div class="card">
-        <img src="${object[i].image}" class="card-img-top" alt=" Fallo al cargar la imagen ${object[i].id}">
-        <div class="card-body">
-            <h5 class="card-title">${object[i].name}</h5>
-            <p class="card-text"> Origin: ${object[i].id}</p>
-            <p id="status" class="card-text"> Status: ${object[i].status}</p>
+        <div class="card">
+            <img src="${object[i].image}" class="card-img-top" alt=" Fallo al cargar la imagen ${object[i].id}">
+            <div class="card-body">
+                <h5 class="card-title">${object[i].name}</h5>
+                <p class="card-text"> Origin: ${object[i].id}</p>
+                <p class="card-text"> Status: ${object[i].status}</p> 
+            </div>
         </div>
-    </div>
     `
     if (typecall===1) return templateCard.appendChild(card);
     
     if (typecall===2) return templateCardPromise.appendChild(card);
 
     if(typecall===3) return templateCardAsync.appendChild(card);
-     
+
+
 }
 
 
