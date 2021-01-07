b54cc0e9e13b773f35d6ec8404089719c7250fd7
Striker Eureka <pablocaceros@gmail.com>
2021-01-07 15:00:36 -0600

    Se le agregaron los estilos CSS

--- js/template.js
+++ js/template.js
@@ -1,18 +1,19 @@
-const templateCard = document.getElementById('card');
+const templateCard = document.getElementById('card1');
 const templateCardPromise = document.getElementById('card2');
 const templateCardAsync = document.getElementById('card3');
+const state = document.getElementById('status');
 
 
 const sendTemplate = (object, i, typecall) => {
     const card = document.createElement('div');
 
     card.innerHTML = `
-    <div class="card border">
+    <div class="card">
         <img src="${object[i].image}" class="card-img-top" alt=" Fallo al cargar la imagen ${object[i].id}">
         <div class="card-body">
             <h5 class="card-title">${object[i].name}</h5>
             <p class="card-text"> Origin: ${object[i].id}</p>
-            <p class="card-text"> Status: ${object[i].status}</p>
+            <p id="status" class="card-text"> Status: ${object[i].status}</p>
         </div>
     </div>
     `
@@ -21,8 +22,6 @@ const sendTemplate = (object, i, typecall) => {
     if (typecall===2) return templateCardPromise.appendChild(card);
 
     if(typecall===3) return templateCardAsync.appendChild(card);
-
-
      
 }
 
